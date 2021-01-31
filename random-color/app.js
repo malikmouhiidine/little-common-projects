1852aa15792ec87cc9c359ad229076dc73402777
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-01-31 17:23:01 +0100

    🔁 Refactoring code

--- random-color/app.js
+++ random-color/app.js
@@ -1,17 +1,17 @@
 function displayCurrentColor() {
-  const body = document.body.style.backgroundImage
-  const title = document.getElementsByTagName('h2')[0]
-  title.textContent = `body { height: 100vh; background-image: ${body} }`
+  const body = document.body.style.backgroundImage;
+  const title = document.getElementsByTagName("h2")[0];
+  title.textContent = `body { height: 100vh; background-image: ${body} }`;
 }
 
-displayCurrentColor()
+displayCurrentColor();
 
 function changeCurrentColor() {
   document.body.style.backgroundImage = `linear-gradient(to bottom right, #${randomHexCodeGenerator()}, #${randomHexCodeGenerator()})`;
-  displayCurrentColor()
+  displayCurrentColor();
 }
 
 function randomHexCodeGenerator() {
-  var randomColor = Math.floor(Math.random()*16777215).toString(16);
-  return randomColor
+  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return randomColor;
 }
