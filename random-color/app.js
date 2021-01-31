dcb607224a36eeec86fdf5b3a73dd9efcf2a3156
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-01-31 14:30:21 +0100

    ➕ random color program

---
+++ random-color/app.js
@@ -0,0 +1,17 @@
+function displayCurrentColor() {
+  const body = getComputedStyle(document.getElementsByTagName('body')[0]).backgroundImage
+  const title = document.getElementsByTagName('h2')[0]
+  title.textContent = `body { height: 100vh; background-image: ${body} }`
+}
+
+displayCurrentColor()
+
+function changeCurrentColor() {
+  document.body.style.backgroundImage = `linear-gradient(to bottom right, #${randomHexCodeGenerator()}, #${randomHexCodeGenerator()})`;
+  displayCurrentColor()
+}
+
+function randomHexCodeGenerator() {
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
+  return randomColor
+}
