81ba8037c7cb32e2d3cf23ac8a3ba325886467d0
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-01 13:31:04 +0100

    ➕ add a download prompt

--- password_generator.py
+++ password_generator.py
@@ -12,4 +12,9 @@ list_of_chosen_chars = []
 for i in range(how_much_char_user_wants):
     list_of_chosen_chars.append(random.choice(
         'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!"#$%&\'()*+,-/:;<=>?@[\\]^_`{|}~'))
-print(''.join(list_of_chosen_chars))
+password_generated = ''.join(list_of_chosen_chars)
+print(password_generated)
+asking_if_user_want_save_password = input(
+    'Do you want to download password to your downloads directory (Y, N): ')
+if (('y' in asking_if_user_want_save_password.lower())):
+    pass
