3782689ebb24c073427a7f28da96e055d2bef091
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-02 16:25:51 +0100

    🎨 Better struct

--- xo-game.py
+++ xo-game.py
@@ -1,16 +1,26 @@
-for i in range(9):
-    game_history = []
+import os
+import random
+game_history = []
 
-    def check_in_game_history():
-        pass
 
-    def Place(n):
+def Place(n):
+    if n in game_history:
+        return 'X'
+    else:
         return n
 
+
+def python_turn():
+    python_choice = random.randint(1, 9)
+
+
+for i in range(9):
     print('   '+str(Place(1))+'|'+str(Place(2))+'|'+str(Place(3)) +
           '\n  -------\n   '
           + str(Place(4))+'|' + str(Place(5))+'|'+str(Place(6)) +
           '\n  -------\n   '
           + str(Place(7))+'|'+str(Place(8))+'|'+str(Place(9)))
-
-    user_turn = int(input())
+    python_turn()
+    user_choice = int(input("You're turn: "))
+    game_history.append(user_choice)
+    os.system('cls' if os.name == 'nt' else 'clear')
