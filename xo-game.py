7bb7b6f2651a66eb72189ad8c530885460b32d8c
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-02 18:40:17 +0100

    🏡 Complete all functions

--- xo-game.py
+++ xo-game.py
@@ -1,26 +1,36 @@
 import os
 import random
-game_history = []
+game_history = {}
 
 
 def Place(n):
     if n in game_history:
-        return 'X'
+        return str(game_history[n])
     else:
-        return n
+        return str(n)
 
 
 def python_turn():
     python_choice = random.randint(1, 9)
+    if not python_choice in game_history:
+        game_history[python_choice] = 'O'
+    else:
+        python_turn()
 
 
-for i in range(9):
-    print('   '+str(Place(1))+'|'+str(Place(2))+'|'+str(Place(3)) +
+for turn in range(4):
+    print('Round: ' + str(turn+1))
+    # print(game_history)  # debug print statement
+    print('   '+Place(1)+'|'+Place(2)+'|'+Place(3) +
           '\n  -------\n   '
-          + str(Place(4))+'|' + str(Place(5))+'|'+str(Place(6)) +
+          + Place(4)+'|' + Place(5)+'|'+Place(6) +
           '\n  -------\n   '
-          + str(Place(7))+'|'+str(Place(8))+'|'+str(Place(9)))
-    python_turn()
+          + Place(7)+'|'+Place(8)+'|'+Place(9))
     user_choice = int(input("You're turn: "))
-    game_history.append(user_choice)
+    if user_choice not in game_history:
+        game_history[user_choice] = 'X'
+    else:
+        print('\nplease make sure to print the right value\n')
+        exit()
+    python_turn()
     os.system('cls' if os.name == 'nt' else 'clear')
