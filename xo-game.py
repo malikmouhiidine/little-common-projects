27a8a98a57182a21f05cba20739f3783279cafd5
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-02 16:04:40 +0100

    🏠 Basic structure

---
+++ xo-game.py
@@ -0,0 +1,16 @@
+for i in range(9):
+    game_history = []
+
+    def check_in_game_history():
+        pass
+
+    def Place(n):
+        return n
+
+    print('   '+str(Place(1))+'|'+str(Place(2))+'|'+str(Place(3)) +
+          '\n  -------\n   '
+          + str(Place(4))+'|' + str(Place(5))+'|'+str(Place(6)) +
+          '\n  -------\n   '
+          + str(Place(7))+'|'+str(Place(8))+'|'+str(Place(9)))
+
+    user_turn = int(input())
