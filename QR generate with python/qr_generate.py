cdc9c689eb3ee1d87c583cccdda55031e5515ad7
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-04 20:17:40 +0100

    🔝 More flexible code

--- QR generate with python/qr_generate.py
+++ QR generate with python/qr_generate.py
@@ -1,6 +1,8 @@
 import qrcode
-image = qrcode.make("https://abdelhamedmouhiidine.wordpress.com/")
-image.save("abdellhamed.png","PNG")
+link = input('Link: ')
+name_file = input('Name the file: ')
+image = qrcode.make(link)
+image.save(name_file + '.png', "PNG")
 
 # image = qrcode.make("url x")
 # image.save("title x.png","PNG")
