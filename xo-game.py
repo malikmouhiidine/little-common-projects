7bf1fc6b4b2f61aa4afb36903f66197cb7efe337
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-05 21:56:57 +0100

    3️⃣ Third part of the smart algorithm

--- xo-game.py
+++ xo-game.py
@@ -1,6 +1,17 @@
 import os
-import random
+import time
 game_history = {}
+winning_combination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+n_places_dict = {}
 
 
 def Place(n):
@@ -22,6 +33,22 @@ def python_turn(turn):
             game_history[last_player_move - 1] = 'O'
         elif not last_player_move + 1 > 8 and not last_player_move + 1 in game_history:
             game_history[last_player_move + 1] = 'O'
+    else:
+        for k, v in game_history.items():
+            n_places_dict.setdefault(v, []).append(k)
+        for n in ['O', 'X']:
+            for i in winning_combination:
+                if all(elem in i for elem in n_places_dict[n]):
+                    for j in i:
+                        if not j in game_history:
+                            game_history[j] = n
+                            return True
+            for i in winning_combination:
+                if any(elem in i for elem in n_places_dict[n][:2]):
+                    for j in i:
+                        if not j in game_history:
+                            game_history[j] = n
+                            return True
 
 
 def check_win():
@@ -37,22 +64,9 @@ def check_win():
 
 
 def Win(n):
-    n_places_dict = {}
     for k, v in game_history.items():
         n_places_dict.setdefault(v, []).append(k)
-    # print(n_places_dict[n])  # debugging print statement
-    winning_combination = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ]
     for i in winning_combination:
-        # print(i)  # debugging print statement
         if all(elem in n_places_dict[n] for elem in i):
             return True
     return False
