39e38aa1ffa067a0d76d42d99c78f731249ee5c3
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-19 16:39:15 +0100

    🎉 Job done (minimax algorithme)

--- xo-game.py
+++ xo-game.py
@@ -20,20 +20,52 @@ def Place(n):
     return str(n)
 
 
-def minimax():
-    return 1
+def best_move(board):
+    available_spots = (
+        available_spot for available_spot in range(0, 9) if available_spot not in board)
+    best_score = -float('inf')
+    for available_spot in available_spots:
+        board[available_spot] = 'O'
+        score = minimax(board, 0, False)
+        if score > best_score:
+            best_score = score
+            move = available_spot
+        del board[available_spot]
+    return move
+
+
+def minimax(board, depth, is_maximazer_turn):
+    if Win('O', board):
+        return 1
+    elif Win('X', board):
+        return -1
+    if len(board) == 9 and not Win('O', board) and not Win('X', board):
+        return 0
+    if is_maximazer_turn:
+        available_spots = (
+            available_spot for available_spot in range(0, 9) if available_spot not in board)
+        best_score = -float('inf')
+        for available_spot in available_spots:
+            board[available_spot] = 'O'
+            score = minimax(board, depth+1, False)
+            best_score = max(score, best_score)
+            del board[available_spot]
+        return best_score
+    else:
+        available_spots = (
+            available_spot for available_spot in range(0, 9) if available_spot not in board)
+        best_score = float('inf')
+        for available_spot in available_spots:
+            board[available_spot] = 'X'
+            score = minimax(board, depth+1, True)
+            best_score = min(score, best_score)
+            del board[available_spot]
+        return best_score
 
 
 def python_turn(turn):
-    if turn == 0:
-        if not 4 in game_history:
-            game_history[4] = 'O'
-        else:
-            game_history[2] = 'O'
-    else:
-        board = game_history.copy()
-        minimax()
-        # game_history[minimax()] = 'O'
+    board = game_history.copy()
+    game_history[best_move(board)] = 'O'
 
 
 def check_win():
@@ -48,12 +80,16 @@ def check_win():
     return True
 
 
-def Win(n):
+def Win(n, game_history=game_history):
+    if len(game_history) == 1:
+        return False
     for k, v in game_history.items():
         n_places_dict.setdefault(v, []).append(k)
     for i in winning_combination:
         if all(elem in n_places_dict[n] for elem in i):
+            n_places_dict.clear()
             return True
+    n_places_dict.clear()
     return False
 
 
@@ -67,16 +103,17 @@ def table():
 
 for turn in range(5):
     print('Round: ' + str(turn+1))
+    python_turn(turn)
+    check_win()
     print(game_history)  # debugging print statement
     table()
-    user_choice = int(input("You're turn: "))
-    if user_choice not in game_history:
-        game_history[user_choice] = 'X'
-    else:
-        print('\nplease make sure to print the right value\n')
-        exit()
     if turn != 4:
-        python_turn(turn)
+        user_choice = int(input("You're turn: "))
+        if user_choice not in game_history and user_choice >= 0 and user_choice <= 8:
+            game_history[user_choice] = 'X'
+        else:
+            print('\nplease make sure to print the right value\n')
+            exit()
     os.system('cls' if os.name == 'nt' else 'clear')
     check_win()
 
